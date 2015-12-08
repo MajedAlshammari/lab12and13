e2e52162f2e31fb8f3ab49d5f27b2d1c1009f1ed
MajedAlshammari <d12125199@mydit.ie>
2015-12-08 23:56:30 +0000

    Delete version.py

--- version.py
+++
@@ -1,9 +0,0 @@
-import boto.sqs
-import boto.sqs.queue
-from boto.sqs.message import Message
-from boto.sqs.connection import SQSConnection
-from boto.exception import SQSError
-
-version = boto.Version
-
-print version

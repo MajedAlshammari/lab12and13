d5532444a3ffad19e3b607bba067110081f90565
MajedAlshammari <d12125199@mydit.ie>
2015-12-09 00:16:23 +0000

    Update verify-boto-and-keys.py

--- myapp/verify-boto-and-keys.py
+++ myapp/verify-boto-and-keys.py
@@ -31,8 +31,8 @@ DELETE		/queues/<qid>/msgs Get and	delete a message	from	the	queue
 
 @app.route("/key" , methods=['GET'])
 def	get_conn():
-key_id,	secret_access_key	=	urllib2.urlopen("http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key").read().split(':')
-return	boto.sqs.connect_to_region("eu-west-1",	aws_access_key_id=key_id,	aws_secret_access_key=secret_access_key)
+    key_id,	secret_access_key	=	urllib2.urlopen("http://ec2-52-30-7-5.eu-west-1.compute.amazonaws.com:81/key").read().split(':')
+    return	boto.sqs.connect_to_region("eu-west-1",	aws_access_key_id=key_id,	aws_secret_access_key=secret_access_key)
 
 
 if __name__ == "__main__":
